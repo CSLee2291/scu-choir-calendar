5a5be008d5fe47c49c1312b9b4033cf3ccdcc07b
CS Lee <156996839+CSLee2291@users.noreply.github.com>
2025-04-03 22:30:17 +0800

    Fix ShareIcon import path in HelpGuide component

---
+++ src/components/icons/CalendarIcon.tsx
@@ -0,0 +1,9 @@
+const CalendarIcon: React.FC<{ className?: string }> = ({ className }) => {
+  return (
+    <svg className={className} /* ... svg attributes ... */>
+      {/* SVG content */}
+    </svg>
+  );
+};
+
+export default CalendarIcon;
